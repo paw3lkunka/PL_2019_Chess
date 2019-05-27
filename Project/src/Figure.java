fd796c7972747a0c1eae2a224b03651c846818e0
Motwg <38580353+Motwg@users.noreply.github.com>
2019-05-27 16:49:34 +0200

    .project dodany
    Mały test

--- Project/src/Figure.java
+++ Project/src/Figure.java
@@ -1,14 +1,11 @@
 import processing.core.PApplet;
 
-public class Figure extends PositionedObject {
+public class Figure {
 
+	protected final PApplet parent;
 	Vector3 color;
 	Vector3 position;
 	
-	public Figure(PApplet parent, Vector3 pos) {
-		super(parent, pos);
-		
-		//...
-	}
+	
 
 }
